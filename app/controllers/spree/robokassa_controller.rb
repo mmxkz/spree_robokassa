ebffe0225d0404b331fd3fd7ce285b6964d33ed0
ovcharik <maksim.ovcharik@gmail.com>
2014-09-05 14:39:39 +0600

    Fixes

--- app/controllers/spree/robokassa_controller.rb
+++ app/controllers/spree/robokassa_controller.rb
@@ -1,7 +1,7 @@
 module Spree
   class RobokassaController < BaseController
     before_action :set_order
-    before_action :validate_request, :only => :result
+    before_action :validate_request, :except => :failure
     skip_before_action :verify_authenticity_token
 
     def result
@@ -10,12 +10,12 @@ module Spree
     end
 
     def success
-      #payment.complete
+      payment.complete
       redirect_to @order, notice: t('robokassa.payment_succeed')
     end
 
     def failure
-      #payment.failure
+      payment.failure
       redirect_to :root, flash: { error: t('robokassa.payment_failed') }
     end
 
@@ -41,9 +41,13 @@ module Spree
     def notification
       req_str = request.query_string
       req_str = request.raw_post if req_str.empty?
+      secret  = action_name == 'result' ?
+        payment.payment_method.preferred_password2 :
+        payment.payment_method.preferred_password1
       @notification ||= ActiveMerchant::Billing::Integrations::Robokassa.notification(
         req_str,
-        secret: payment.payment_method.preferred_password2)
+        secret: secret)
+      @notification
     end
   end
 end
